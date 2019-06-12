b1a51bbc2c0ccd6b2a002846c52117cb3abe4f2b
Nagendra Singh <nagendras.chahar@gmail.com>
2019-06-12 11:18:37 +0530

    validation

--- main.js
+++
@@ -1,8 +0,0 @@
-function validateForm(){
-    var psw = document.forms["register"]["psw"].value;
-    var pswRepeat = document.forms["register"]["psw-repeat"].value;
-    if (psw != pswRepeat) {
-        alert("psw and psw-repeat must be same");
-        return false;
-    }
-}
