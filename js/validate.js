beaf193422213e852595ccff94804489d544d3c7
Nagendra Singh <nagendras.chahar@gmail.com>
2019-06-15 00:21:09 +0530

    balel and webpack configuration

--- js/validate.js
+++ js/validate.js
@@ -1,9 +1,7 @@
 (function getSession(){
-
   if(localStorage.getItem("userData") != null){
     document.querySelector(".main_container").classList.add("login")
   }
-
 })();
 
 function validateSpecialCharacter(value){
@@ -11,48 +9,53 @@ function validateSpecialCharacter(value){
 }
 
 function validateEmail(emailField){
-  var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  let reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
   return reg.test(emailField);
 }
 
 function clearValidation(){
-  var y = document.querySelectorAll(".form-control");
-  for (var l = 0; l < y.length; l++) {
-    y[l].parentElement.classList.remove("error-group");
+  
+  let input = document.querySelectorAll(".invalid-feedback");
+  for (let l = 0; l < input.length; l++) {
+    input[l].parentNode.removeChild(input[l]);
   }
 }
 
 function showAlert(el, message){
   el.parentElement.classList.add("error-group");
-  el.parentElement.children[2].innerHTML = message;
+  let mszEl = document.createElement("div");
+  mszEl.className = "invalid-feedback";
+  let textMsz = document.createTextNode(message);
+  mszEl.appendChild(textMsz);
+  el.parentElement.appendChild(mszEl);
   return false;
 }
 
 function validateForm(){
   clearValidation();
-  var input = document.querySelectorAll(".required");
-  for (var i = 0; i < input.length; i++) {
+  let input = document.querySelectorAll(".required");
+  for (let i = 0; i < input.length; i++) {
     if(input[i].value === ''){
      return showAlert(input[i], input[i].name+' must be filled out')
     }
   }
 
-  var special = document.querySelectorAll(".not-special");
-  for (var i = 0; i < special.length; i++) {
+  let special = document.querySelectorAll(".not-special");
+  for (let i = 0; i < special.length; i++) {
     if(validateSpecialCharacter(special[i].value)){
       return showAlert(special[i], 'Please fill valid ' + special[i].name)
     }
   }
 
-  var email = document.querySelectorAll(".email");
-  for (var i = 0; i < email.length; i++) {
+  let email = document.querySelectorAll(".email");
+  for (let i = 0; i < email.length; i++) {
     if(!validateEmail(email[i].value)){
       return showAlert(email[i], 'Please fill valid ' + email[i].name)
     }
   }
 
-  var password = document.forms["register"]["password"];
-  var pswConfirm = document.forms["register"]["pswConfirm"];
+  let password = document.forms["register"]["password"];
+  let pswConfirm = document.forms["register"]["pswConfirm"];
 
   if (password.value != pswConfirm.value) {
     return showAlert(pswConfirm, 'Password and Confirm Password must be same')
@@ -64,7 +67,7 @@ function validateForm(){
 document.getElementById("registerForm").addEventListener("submit", function(event){
   event.preventDefault()
   if(validateForm()){
-    var userData = {
+    const userData = {
       userName: document.forms["register"]["userName"].value,
       email: document.forms["register"]["email"].value,
       password: document.forms["register"]["password"].value,
@@ -72,7 +75,6 @@ document.getElementById("registerForm").addEventListener("submit", function(even
     }
 
     localStorage.setItem('userData', JSON.stringify(userData));
-    
     document.querySelector(".main_container").classList.add("login")
     
   }
